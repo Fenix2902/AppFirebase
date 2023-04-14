ea72269301caef76609856069dda05043de7899f
afalarco22 <afalarco@unal.edu.co>
2023-04-14 08:51:35 -0500

    Se realiza la conexión con firebase, y se configura boton guardar para ingresar usuarios

--- app/src/main/java/com/example/appfirebase/MainActivity.java
+++ app/src/main/java/com/example/appfirebase/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.appfirebase;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    EditText etName, etUsername, etPassword;
+    Button btSave, btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.etName);
+        etPassword = findViewById(R.id.etPassword);
+        btSave = findViewById(R.id.btnSave);
+        btnSearch = findViewById(R.id.btnSearch);
+        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new user with a first and last name
+                Map<String, Object> user = new HashMap<>();
+                user.put("name", etName.getText().toString());
+                user.put("username", etUsername.getText().toString());
+                user.put("password", etPassword.getText().toString());
+
+                // Add a new document with a generated ID
+                db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(getApplicationContext(),"Usuario creado correctamente con id: "+documentReference.getId(),Toast.LENGTH_SHORT).show();
+                                //Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(), "Error al crear el usuario: "+e, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
     }
 }
