4794286ec942fb1e4c6bcfb97fdfcb595b92835e
Fenix2902 <alexandercalleagudelo@gmail.com>
2023-05-02 09:52:46 -0500

    registro de usuario

---
+++ app/src/main/java/com/example/appfirebase/MainActivity_Opciones_Registro.java
@@ -0,0 +1,15 @@
+package com.example.appfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_Opciones_Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_opciones_registro);
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/example/appfirebase/MainActivity_Registro_Usuario.java
@@ -0,0 +1,15 @@
+package com.example.appfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_Registro_Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_registro_usuario);
+        getSupportActionBar().hide();
+    }
+}
